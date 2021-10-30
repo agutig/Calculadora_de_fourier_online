de350114dffa38e2fed6e9ec301f0c2bb761fc53
de4dcry <hodeadmusic@gmail.com>
2021-10-30 15:29:14 +0200

    letsgoo

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Obtener una referencia al elemento canvas del DOM
-const $grafica = document.querySelector("#grafica");
+var grafica = document.getElementById("grafica").getContext("2d");
 const text_block = document.getElementById("temp_sing");
 const ready = document.getElementById("ready_butt");
 var check_text = document.getElementById("check_text");
@@ -8,29 +8,30 @@ var singnal_text = ""
 ready.onclick = function(){
     singnal_text = text_block.value;
     check_text.innerHTML = "y(t)= " + singnal_text;
-    const signal = new signal(singnal_text);
-
+    const signal = new signal_char(singnal_text);
 // Las etiquetas son las que van en el eje X. 
-    var x_axis = signal.x_axis();
+    var x_axis = signal.x_axis;
 
 // Podemos tener varios conjuntos de datos. Comencemos con uno
 
 var y_axis = {
     label: "Amplitud",
-    data: signal.y_axis(), // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
-
+    data: signal.y_axis, // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
+    
 // Editar la grafica	
            backgroundColor: 'rgba(54, 162, 235, 0.2)', // Color de fondo
            borderColor: 'rgba(54, 162, 235, 1)', // Color del borde
            borderWidth: 1,// Ancho del borde
 };
 
-new Chart($grafica, {
+
+new Chart(grafica, {
     type: 'line',// Tipo de gráfica
     data: {
         labels: x_axis,
         datasets: [
             y_axis
+
             // Aquí más datos...
         ]
     },
@@ -44,5 +45,7 @@ new Chart($grafica, {
         },
     }
 });
+
+
 
 }
--- signal_prepare.js
+++ signal_prepare.js
@@ -1,36 +1,56 @@
 
-class signal {
+class signal_char {
 
     constructor ( signal_text ){
-        this.x_axis = prepare_axis_x()
-        this.y_axis = prepare_axis_y(signal_text)
+        this.x_axis = this.prepare_axis_x()
+        this.y_axis = this.prepare_axis_y(signal_text)
       }
 
-    static prepare_axis_y(signal) {
+    prepare_axis_y(signal) {
         var x_axis = this.prepare_axis_x()
         var y_axis = []
         var value = 0
-        for ( t = x_axis[0]; t < x_axis[x_axis.length - 1]; step++) {
-            value = ToNumber(singnal)
+        // el mas x++ será sustituido por la frecuencia de muestreo que a ver como la saco
+        for ( let x = x_axis[0]; x <= x_axis[x_axis.length - 1]; x++) {
+
+            var sig_cal = this.especial_funcs(signal)
+            sig_cal = sig_cal.replace('x' , "(" + x + ")")
+            value = eval(sig_cal)
+            console.log(sig_cal)
+            console.log(value)
             y_axis.push(value)
           }
         return y_axis
     }
-    static prepare_axis_x(){
+    prepare_axis_x(){
         /// Hacer una funcion que calcule el ancho de banda de la señal.
+         //Si es definida --> Que muestre su tamaño + la mitad de su tamaño por la derecha + la mitad de su tamaño por la izquierda
+         //Si es periodica --> Que muestre dos periodos por alante y dos por detras (revisar autocorrelacion)
+         //si es infinita en ancho de banda --> +-1000s
+         
         var x_axis = []
-        for (let x = -100; x < 100; step++) {
+        for (let x = -100; x < 100; x++) {
             x_axis.push(x);
         }
         return x_axis
     }
 
-    x_axis(){
-        return this.x_axis
+    especial_funcs(text){
+        var text_changed = text
+        text_changed = text_changed.replaceAll('^' , ' ** ')
+        text_changed = text_changed.replaceAll('cos' , 'Math.cos')
+        text_changed = text_changed.replaceAll('sin' , 'Math.sin')
+        text_changed = text_changed.replaceAll('sen' , 'Math.sin')
+        text_changed = text_changed.replaceAll('e' , 'Math.E')
+        text_changed = text_changed.replaceAll('abs' , 'Math.abs')
+        text_changed = text_changed.replaceAll('pi' , 'Math.PI')
+        text_changed = text_changed.replaceAll('ln' , 'Math.log') //log nepe
+        text_changed = text_changed.replaceAll('log' , 'Math.log10') //log base 10
+        // meter el resto: https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Math/log10
+
+        return text_changed;
     }
 
-    y_axis(){
-        return this.y_axis
-    }
+
 
 }
